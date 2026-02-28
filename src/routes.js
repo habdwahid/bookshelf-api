6c5cf7d8b414e9e6e8f2223d02a0cb63834180f9
Hasbi <habdwaheed@gmail.com>
2026-02-28 21:29:42 +0700

    feat: Create routes and connect it to controller

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import {createBook, deleteBookById, getBookById, getBooks, updateBookById} from './controller'
+
+// Router initialization
+const router = express.Router()
+
+// Display books route
+router.get('/books', getBooks)
+
+// Create book route
+router.post('/books', createBook)
+
+// Get book by id route
+router.get('/books/:id', getBookById)
+
+// Update book by id route
+router.put('/books/:id', updateBookById)
+
+// Delete book route
+router.delete('/book/:id', deleteBookById)
+
+export default router
